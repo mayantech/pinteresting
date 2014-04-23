aeb559beccc25b6237c1dd47673d403bd82db35e
Carlos Ramirez <carlos.ramirez@mayan-tech.com>
2014-04-23 15:20:48 -0600

    change the scaffolds css

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -11,14 +11,14 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build#Pin.new
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params) #current_user.pin.build(pin_params)
+    @pin = current_user.pins.build(pin_params)#Pin.new(pin_params) #current_user.pin.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422204852) do
+ActiveRecord::Schema.define(version: 20140423170210) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
